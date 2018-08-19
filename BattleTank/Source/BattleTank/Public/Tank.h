962affaa5ab0560681bf2f8402f2a3c9ccf9295d
Karen Li <carrepbearep@gmail.com>
2018-08-18 22:44:27 -0700

    Lecture 131

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -15,11 +15,9 @@ public:
 	// Sets default values for this pawn's properties
 	ATank();
 
-protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
